6f6da093b21b47f70c1a848d1b4b26502ebf807f
Anuar Orozco <anuarorozzco@gmail.com>
2024-11-25 15:46:26 -0600

    Init commit

---
+++ src/main/java/com/uvm/control_escolar/SistemaDeControlEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.uvm.control_escolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeControlEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeControlEscolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uvm/control_escolar/SistemaDeControlEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uvm.control_escolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeControlEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
