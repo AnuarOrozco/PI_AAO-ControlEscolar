163b417d2575e5ac423063151df4b9226bab61cd
Anuar Orozco <anuarorozzco@gmail.com>
2024-11-25 18:30:11 -0600

    Created StudentController

---
+++ src/main/java/com/uvm/control_escolar/controller/StudentController.java
@@ -0,0 +1,68 @@
+package com.uvm.control_escolar.controller;
+
+import com.uvm.control_escolar.entity.Student;
+import com.uvm.control_escolar.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable Long id) {
+        Student student = studentService.getStudentById(id);
+
+        if (student != null) {
+            return ResponseEntity.ok(student);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllStudents() {
+        return ResponseEntity.ok(studentService.getAllStudents());
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createStudent(@RequestBody Student student) {
+        Student createdStudent = studentService.createStudent(student);
+
+        // Devuelve 201 Created con la URI del recurso
+        return ResponseEntity.created(URI.create("/students/" + createdStudent.getId())).body(createdStudent);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateStudent(@PathVariable Long id, @RequestBody Student student) {
+        Student existingStudent = studentService.getStudentById(id);
+
+        if (existingStudent != null) {
+            student.setId(id);
+            ResponseEntity<?> updatedStudent = studentService.updateStudent(id, student); // Si da error es por esto
+
+            return ResponseEntity.ok(updatedStudent);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteStudent(@PathVariable Long id) {
+        Student searchStudent = studentService.getStudentById(id);
+
+        if (searchStudent != null) {
+            studentService.deleteStudent(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+}
