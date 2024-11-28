422e5eba947c7277ec49832c642fcf13fa47a6ef
Anuar Orozco <anuarorozzco@gmail.com>
2024-11-27 22:49:49 -0600

    Added an animation to fade out the confirmation messageg

--- src/main/java/com/uvm/control_escolar/controller/StudentController.java
+++ src/main/java/com/uvm/control_escolar/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/students")
@@ -21,9 +22,9 @@ public class StudentController {
     }
 
     @PostMapping("/form")
-    public String saveStudent(@ModelAttribute("student") Student student) {
+    public String saveStudent(@ModelAttribute("student") Student student, RedirectAttributes redirectAttributes) {
         studentService.createStudent(student);
-
+        redirectAttributes.addFlashAttribute("message", "Estudiante dado de alta exitosamente!");
         return "redirect:/students/form";
     }
 
